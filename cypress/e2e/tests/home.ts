97698194ad9b271222102560edda0212c8a519a8
unknown <sabbir.ul.alam.sabbir@gmail.com>
2025-05-22 00:38:59 +0600

    Add test to verify pathname i.e url path

--- cypress/e2e/tests/home.ts
+++ cypress/e2e/tests/home.ts
@@ -4,13 +4,46 @@ describe("home page", () => {
     cy.visit("http://localhost:3000")
   })
 
-  it.only("the h1 contains the correct text", () => {
-    // cy.visit("http://localhost:3000")
-    cy.getByData("hero-heading").contains("Testing Next.js Applications with Cypress")
+  context("Hero Section", () => {
+    it("the h1 contains the correct text", () => {
+      // cy.visit("http://localhost:3000")
+      cy.getByData("hero-heading").contains("Testing Next.js Applications with Cypress")
+    })
+
+    it("the features on the homepage are correct", () => {
+      // cy.visit("http://localhost:3000")
+      cy.get("dt").eq(0).contains("4 Courses")
+    })
+
   })
 
-  it("the features on the homepage are correct", () => {
-    // cy.visit("http://localhost:3000")
-    cy.get("dt").eq(0).contains("4 Courses")
+  context("Courses section",()=>{
+    it("course: testing your first nextJs app",()=>{
+      cy.getByData('course-0').find('a')
+      .contains("Get started").click()
+
+      cy.location("pathname")
+      .should('equal',"/testing-your-first-application")
+    })
+
+    it("course: testing foundations",()=>{
+      cy.getByData('course-1').find('a')
+      .contains('Get started').click()
+
+      cy.location("pathname")
+      .should("equal","/testing-foundations")
+
+
+    })
+
+    it.only('course: cypress fundamentals',()=>{
+      cy.getByData('course-2').find('a')
+      .contains("Get started").click()
+
+      cy.location("pathname")
+      .should("equal","/cypress-fundamentals")
+    })
+
+
   })
 })
