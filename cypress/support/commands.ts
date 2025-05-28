18c83465d2e3e8e7509515d913d6d6888d563204
unknown <sabbir.ul.alam.sabbir@gmail.com>
2025-05-28 23:37:49 +0600

    Add feature file for sign-in page

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -34,40 +34,40 @@ Cypress.Commands.add("getByData", (selector) => {
     return cy.get(`[data-test=${selector}]`)
 })
 
-Cypress.Commands.add("createTransaction", (payload) => {
-    const log = Cypress.log({
-        name: "createTransaction",
-        displayName: "CREATE TRANSACTION",
-        message: [
-            `(${payload.transactionType}) : ${payload.sender.id} and ${payload.receiver.id}`,
+// Cypress.Commands.add("createTransaction", (payload) => {
+//     const log = Cypress.log({
+//         name: "createTransaction",
+//         displayName: "CREATE TRANSACTION",
+//         message: [
+//             `(${payload.transactionType}) : ${payload.sender.id} and ${payload.receiver.id}`,
 
-        ],
-        autoEnd: false,
-        consoleProps() {
-            return payload
-        }
-    })
-    return cy
-        .window({ log: false })
-        .then((win) => {
-            log.snapshot("before")
-            win.createTransactionService.send("SET_USERS", payload)
+//         ],
+//         autoEnd: false,
+//         consoleProps() {
+//             return payload
+//         }
+//     })
+//     return cy
+//         .window({ log: false })
+//         .then((win) => {
+//             log.snapshot("before")
+//             win.createTransactionService.send("SET_USERS", payload)
 
-            const createPayload = pick(
-                ["amount", "description", "transactionType"],
-                payload
-            )
+//             const createPayload = pick(
+//                 ["amount", "description", "transactionType"],
+//                 payload
+//             )
 
-            return win.createTransactionService.send("CREATE", {
-                ...createPayload,
-                senderId: payload.sender.id,
-                receiverId: payload.receiver.id,
-            })
-        })
-        .then(() => {
-            log.snapshot("after")
-            log.end()
+//             return win.createTransactionService.send("CREATE", {
+//                 ...createPayload,
+//                 senderId: payload.sender.id,
+//                 receiverId: payload.receiver.id,
+//             })
+//         })
+//         .then(() => {
+//             log.snapshot("after")
+//             log.end()
 
 
-        })
-})
+//         })
+// })
